fba58120b3a206d96ba68f39d06482002c7d1088
Atif Jamal Ansari <atifjamalansari@outlook.com>
2021-03-04 21:41:22 +0530

    Portfolio Redesigned

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,6 @@
     
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
     },
 
     $WIN = $(window);
@@ -43,6 +42,34 @@
         
         });
     };
+
+
+    /* Niceccroll
+    * -------------------------------------------------- */
+    function IEVersion() { //Check IE11
+        if (!!navigator.userAgent.match(/Trident\/7\./)) {
+            return 11;
+        }
+    }
+
+    if (IEVersion() != 11) {
+        var clNiceScroll = function(){
+            var niceScroll = $("body").niceScroll({
+                cursorcolor: "#111111",
+                zindex: '99999',
+                cursorminheight: 60,
+                scrollspeed: 80,
+                cursorwidth: 7,
+                autohidemode: false,
+                background: "#aaa",
+                cursorborder: 'none',
+                cursoropacitymax: .7,
+                cursorborderradius: 0,
+                horizrailenabled: false,
+                autohidemode: false,
+            });
+        };
+    }
 
 
    /* Menu on Scrolldown
@@ -360,7 +387,7 @@
                     maxlength: "your number must not exeed 15 digit limit"
                 },
                 contactMessage: {
-                    required: "um...yea, you have to write something to send this form.",
+                    required: "um..., you have to write something to send this form.",
                     minlength: "thats all? really?",
                     maxlength: "are you sure it is just a message?"
                 }
@@ -427,39 +454,6 @@
     };
 
 
-   /* AjaxChimp
-    * ------------------------------------------------------ */
-    var clAjaxChimp = function() {
-        
-        $('#mc-form').ajaxChimp({
-            language: 'es',
-            url: cfg.mailChimpURL
-        });
-
-        // Mailchimp translation
-        //
-        //  Defaults:
-        //	 'submit': 'Submitting...',
-        //  0: 'We have sent you a confirmation email',
-        //  1: 'Please enter a value',
-        //  2: 'An email address must contain a single @',
-        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
-        //  5: 'This email address looks fake or invalid. Please enter a real email address'
-
-        $.ajaxChimp.translations.es = {
-            'submit': 'Submitting...',
-            0: '<i class="fas fa-check"></i> We have sent you a confirmation email',
-            1: '<i class="fas fa-warning"></i> You must enter a valid e-mail address.',
-            2: '<i class="fas fa-warning"></i> E-mail address is not valid.',
-            3: '<i class="fas fa-warning"></i> E-mail address is not valid.',
-            4: '<i class="fas fa-warning"></i> E-mail address is not valid.',
-            5: '<i class="fas fa-warning"></i> E-mail address is not valid.'
-        } 
-
-    };
-
-
    /* Back to Top
     * ------------------------------------------------------ */
     var clBackToTop = function() {
@@ -486,6 +480,7 @@
     (function ssInit() {
         
         clPreloader();
+        clNiceScroll();
         clMenuOnScrolldown();
         clOffCanvas();
         clPhotoswipe();
@@ -496,7 +491,6 @@
         clAlertBoxes();
         clContactForm();
         clAOS();
-        clAjaxChimp();
         clBackToTop();
 
     })();
